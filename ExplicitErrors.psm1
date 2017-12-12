326e863cf3ddfca946056692e0f0b2e788fa7697
Paul H Cassidy <qawarrior@gmail.com>
2017-12-11 17:06:45 -0800

    added skeleton of new cmd

--- ExplicitErrors.psm1
+++ ExplicitErrors.psm1
@@ -1,5 +1,8 @@
 $moduleRoot = $PSScriptRoot
 
+Set-Variable -Name ErrorActionPreference -Value SilentlyContinue -Scope Global -Force
+Set-Variable -Name PSDefaultParameterValues -Value @{"*:ErrorAction"='SilentlyContinue'; "*:ErrorVariables"='ExErr'} -Scope Global -Force
+
 . "$ModuleRoot\Lib\New-ExErrRecord"
 
 Export-ModuleMember -Function "New-ExErrRecord"
