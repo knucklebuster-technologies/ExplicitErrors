4f47cfa3c2da6a85f8899961256d81ffe81cb1dd
Paul H Cassidy <qawarrior@gmail.com>
2017-12-11 20:32:08 -0800

    Updating This module with a commit and backup feature

--- Lib/New-ExErrRecord.Tests.ps1
+++ Lib/New-ExErrRecord.Tests.ps1
@@ -2,8 +2,8 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "New-ErrorRecord" {
-    $Err = New-ErrorRecord -ErrorDescription "Oh Oh!" -ErrorId "Test 1" -ErrorCategory InvalidArgument -CallStack $(Get-PSCallStack) -ErrorAction SilentlyContinue
+Describe "New-ExErrRecord" {
+    $Err = New-ExErrRecord -ErrorDescription "Oh Oh!" -ErrorId "Test 1" -ErrorCategory InvalidArgument -CallStack $(Get-PSCallStack) -ErrorAction SilentlyContinue
     It "ErrorRecord was created" {
         $Err | Should Not Be $null
     }
---
+++ Lib/Write-ExErrRecord.ps1
@@ -0,0 +1,59 @@
+﻿using namespace System.Management.Automation
+
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+function Write-ExErrRecord {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, HelpMessage='The ErrorRecord to be added')]
+        [ValidateNotNullOrEmpty()]
+        [ErrorRecord]
+        $ErrorRecord,
+        [Parameter(Mandatory=$true, ParameterSetName='Text')]
+        [Parameter(Mandatory=$true, ParameterSetName='XML')]
+        [Parameter(Mandatory=$true, ParameterSetName='JSON')]
+        [ValidateScript({ Test-Path -Path $PSItem })]
+        [string]
+        $Path,
+        [Parameter(Mandatory=$true, ParameterSetName='Text')]
+        [Parameter(Mandatory=$true, ParameterSetName='XML')]
+        [Parameter(Mandatory=$true, ParameterSetName='JSON')]
+        [string]
+        $Name,
+        [Parameter(Mandatory=$true, ParameterSetName='Text')]
+        [switch]
+        $Text,
+        [Parameter(Mandatory=$true, ParameterSetName='XML')]
+        [switch]
+        $XML,
+        [Parameter(Mandatory=$true, ParameterSetName='JSON')]
+        [switch]
+        $JSON
+    )
+    
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'Text' {}
+            'XML' {}
+            'JSON' {}
+            Default {
+                $PSItem | Write-Error
+            }
+        }
+    }
+}
+
+$ExportFunction+='Write-ExErrRecord'
